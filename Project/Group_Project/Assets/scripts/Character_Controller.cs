73b023d96cfcfe7caede88d5e4ad9c4cf25caa0a
dannybeales <s185384@ucs.ac.uk>
2016-10-26 00:34:17 +0100

    Added More rotation & thrust control
    Added more rotation control and thrust control.little bit messy at the
    moment

--- Project/Group_Project/Assets/scripts/Character_Controller.cs
+++ Project/Group_Project/Assets/scripts/Character_Controller.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Character_Controller : MonoBehaviour {
 
-
+    //with thrust im assuming it is based in M p/s as thats what Unity works in. 
     public float thrust = 0.5f;
     public Rigidbody2D rb;
 
@@ -28,32 +28,41 @@ public class Character_Controller : MonoBehaviour {
 
     }
 
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+   //Float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+   // {
+       // return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
 
-    }
+   // }
 
 
 
      void Update()
     {
 
+
+        //  IGNORE ALL OF THIS, TRYING OUT ROTATIONS
+
         //get the screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        // Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
 
         //get the screen positon of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        // Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
         //get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        // float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
 
 
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+      //  Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+       // Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+       // lookPos = lookPos - transform.position;
+       // float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+       // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
 
-    Vector3 lastPosition = Vector3.zero;
+  //  Vector3 lastPosition = Vector3.zero;
 
 
     void FixedUpdate()
@@ -63,37 +72,50 @@ public class Character_Controller : MonoBehaviour {
 
         if (Input.GetKey(up_Key))
         {
-            rb.AddForce(Vector2.up * thrust);            
+            rb.AddForce(Vector2.up * thrust);
+            Terminal_Velocity();
         }
         else if (Input.GetKey(down_Key))
         {
             rb.AddForce(Vector2.down * thrust);
+            Terminal_Velocity();
         }
         else if (Input.GetKey(right_Key))
         {
             rb.AddForce(Vector2.right * thrust);
+            Terminal_Velocity();
         }
         else if (Input.GetKey(left_Key))
         {
-            // rb.AddForce(Vector2(-1, 0) * 10);
+            // rb.AddForce(Vector2(-1, 0) * 10); <- this is needed for more ease of directional flight
             rb.AddForce(Vector2.left * thrust);
+            Terminal_Velocity();
         }
 
 
 
 
         //trying to display a speed per second kinda thnig. Not working yet D.B
-        speed = (transform.position - lastPosition).magnitude;
-        lastPosition = transform.position;
-        score_Text.text = speed + "km p/h";
+       // speed = (transform.position - lastPosition).magnitude;
+      //  lastPosition = transform.position;
+      //  score_Text.text = speed + "km p/h";
 
 
 
 
 
     }
+    public void Terminal_Velocity()
+    {
+        if (thrust != 100)
+        {
+            thrust = thrust + 0.5f; //if thrust is not to high carry on increasing, Mayabe turn this into Termial Veloicy or some sort?
+        }
 
+
+
+    }
     // Update is called once per frame
-   
+
 
 }
