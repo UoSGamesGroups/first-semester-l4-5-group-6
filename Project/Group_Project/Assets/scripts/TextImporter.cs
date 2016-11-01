2b046f9c9e7a79635399214d6bb17b226d9717bc
moccy <elliot+github@dewhurst.me>
2016-11-01 21:53:39 +0000

    Added baseline for dialog system

---
+++ Project/Group_Project/Assets/scripts/TextImporter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextImporter : MonoBehaviour
+{
+
+    public TextAsset TextFile;
+    private string[] TextLines;
+
+	// Use this for initialization
+	void Start () {
+        // Making sure the text file is assigned in the editor.
+	    if (TextFile != null)
+	    {
+            // Assigns TextLines to each line in the text file. \n represents a newline character
+            // so we are creating a collection of text split up by each newline.
+	        TextLines = TextFile.text.Split('\n');
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(Controls.ActionKey))
+	    {
+	        
+	    }
+	}
+}
