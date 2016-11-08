5dcf2f02bb5d3a6d708762de442f836d2ee25bc1
dannybeales <s185384@ucs.ac.uk>
2016-11-08 23:27:18 +0000

    Started work on Puzzle - 2D - Cubes, Ridged Bodies,
    More work was done in Unity rather than the code. A fair few bits of set
    up for future stuff. Needs more work.
    
    Loads of commented out code as im experiementing, will clean up once i
    have found a working solution.

--- Project/Group_Project/Assets/scripts/Character_Controller.cs
+++ Project/Group_Project/Assets/scripts/Character_Controller.cs
@@ -1,121 +1,69 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Character_Controller : MonoBehaviour {
-
-    //with thrust im assuming it is based in M p/s as thats what Unity works in. 
-    public float thrust = 0.5f;
-    public Rigidbody2D rb;
-
-
-    //Speed
-    public float speed = 0;
-
-    //Control Strings
-    public string up_Key;
-    public string down_Key;
-    public string left_Key;
-    public string right_Key;
-   
-
-    private GUIText score_Text;
-
-	// Use this for initialization
-	void Start () {
-
-        rb = GetComponent<Rigidbody2D>();
-
-
-    }
-
-   //Float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-   // {
-       // return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-
-   // }
+    
+    private Rigidbody2D rb;
+    private Rigidbody2D cuberb;
+    public KeyCode UpKey, DownKey, LeftKey, RightKey;
+    public float MaxVelocity;
+    public Text velocityText;
+    [Range(1f, 100f)]
+    public float Velocity = 10f;
 
 
 
-     void Update()
+    public AudioClip PropellantJets;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
     {
-
-
-        //  IGNORE ALL OF THIS, TRYING OUT ROTATIONS
-
-        //get the screen positions of the object
-        // Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
-
-        //get the screen positon of the mouse
-        // Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //get the angle between the points
-        // float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-
-        //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-
-      //  Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-       // Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
-       // lookPos = lookPos - transform.position;
-       // float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
-       // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
     }
 
-
-  //  Vector3 lastPosition = Vector3.zero;
-
-
-    void FixedUpdate()
-    {
-
-        
-
-        if (Input.GetKey(up_Key))
-        {
-            rb.AddForce(Vector2.up * thrust);
-            Terminal_Velocity();
-        }
-        else if (Input.GetKey(down_Key))
-        {
-            rb.AddForce(Vector2.down * thrust);
-            Terminal_Velocity();
-        }
-        else if (Input.GetKey(right_Key))
-        {
-            rb.AddForce(Vector2.right * thrust);
-            Terminal_Velocity();
-        }
-        else if (Input.GetKey(left_Key))
-        {
-            // rb.AddForce(Vector2(-1, 0) * 10); <- this is needed for more ease of directional flight
-            rb.AddForce(Vector2.left * thrust);
-            Terminal_Velocity();
-        }
-
-
-
-
-        //trying to display a speed per second kinda thnig. Not working yet D.B
-       // speed = (transform.position - lastPosition).magnitude;
-      //  lastPosition = transform.position;
-      //  score_Text.text = speed + "km p/h";
-
-
-
-
-
-    }
-    public void Terminal_Velocity()
-    {
-        if (thrust != 100)
-        {
-            thrust = thrust + 0.5f; //if thrust is not to high carry on increasing, Mayabe turn this into Termial Veloicy or some sort?
-        }
-
-
-
-    }
     // Update is called once per frame
+    void Update()
+    {
+        // TODO: Make movement keys adjustable by Designers.
+        if (Input.GetKey(KeyCode.W)) // Up
+        {
+            rb.AddForce(Vector2.up * Time.deltaTime * Velocity * 1000);
+            source.PlayOneShot(PropellantJets);
+        }
+        if (Input.GetKey(KeyCode.S)) // Down
+        {
+            rb.AddForce(Vector2.down * Time.deltaTime * Velocity * 1000);
+        }
+        if (Input.GetKey(KeyCode.A)) // Left 
+        {
+            rb.AddForce(Vector2.left * Time.deltaTime * Velocity * 10000);
+        }
+        if (Input.GetKey(KeyCode.D)) // Right
+        {
+            rb.AddForce(Vector2.right * Time.deltaTime * Velocity * 10000);
+        }
+
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxVelocity);
+
+        // Debug velocity text.
+        //TODO: Move this to a UI Controller.
+        velocityText.text = "Velocity X: " + rb.velocity.x + "\n"
+                          + "Velocity Y: " + rb.velocity.y + "\n"
+                          + "Magnitude: " + rb.velocity.magnitude;
+    }
+
+    void OnCollisionEnter2D(Collider cube)
+    {
+
+        if (cube.gameObject.tag.Equals("xblock") == true)
+        {
+            return;
+        }
+    }
+
 
 
 }
---
+++ Project/Group_Project/Assets/scripts/Collision_Controller.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision_Controller : MonoBehaviour {
+
+
+    private Rigidbody2D cuberb;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+	}
+
+    void OnCollisionEnter2D(Collider cube)
+    {
+
+        if (cube.gameObject.tag.Equals("switch") == true)
+        {
+            return;
+        }
+    }
+
+
+
+
+    //void onCollisionEnter(Collision c)
+    //{
+    //    float force = 100;
+
+    //    if (c.gameObject.tag == "block")
+    //    {
+    //        cuberb = gameObject.GetComponent<Rigidbody2D>();
+    //        Vector3 dir = c.contacts[0].point - transform.position;
+
+    //        dir = dir.normalized;
+
+    //        GetComponent<Rigidbody>().AddForce(dir * force);
+
+
+
+    //    }
+
+    //}
+
+
+    //public float pushPower = 2.0f;
+    //void OnControllerColliderHit(ControllerColliderHit hit)
+    //{
+    //    Rigidbody body = hit.collider.attachedRigidbody;
+
+    //    //if (body == null || body.velocity)
+    //    //{
+    //    //    return;
+    //    //}
+    //    //if (hit.moveDirection.y < -0.3f)
+    //    //{
+    //    //    return; 
+    //    //}
+    //    Vector3 pushDir = new Vector2(hit.moveDirection.x, 0);
+    //    body.velocity = pushDir * pushPower;
+
+
+    //}
+}
