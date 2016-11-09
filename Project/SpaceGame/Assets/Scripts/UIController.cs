c88bc35fcc7503532a9f0d179da38f20560fec98
Elliot <elliot+github@dewhurst.me>
2016-11-09 16:26:56 +0000

    3D Movement Prototype Done

---
+++ Project/SpaceGame/Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    public Text VelocityText;
+    private GameObject _player;
+    private Rigidbody _playerRB;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
+	    _playerRB = _player.GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    VelocityText.text = "<b>Velocity</b>\n" +
+	                        "F: " + _player.transform.InverseTransformDirection(_playerRB.velocity).z.ToString("F") + "\n" + 
+                            "S: " + Mathf.Abs(_player.transform.InverseTransformDirection(_playerRB.velocity).x).ToString("F");
+	}
+}
