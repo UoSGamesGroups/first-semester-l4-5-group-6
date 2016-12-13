de396a31dbd4587cbd01ae401aa150ed8293d0d4
dannybeales <s185384@ucs.ac.uk>
2016-12-13 22:40:40 +0000

    wave system manager,
    Does not work that well, Is obsolete, but has some usefull progress in there.

---
+++ waveManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class waveManager : MonoBehaviour {
+
+    public float waveNumber;
+    public float firstTimer = 30.0f;
+
+    public Text WaveCountText;
+    public Text TimerText;
+    public Text WaveStartText;
+
+    public bool WaveActive;
+
+
+    public float timeLeft;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown("o"))
+        {
+            //WaveActive = true;
+        }
+
+        if (WaveActive == true)
+        {
+            waveNumber++;
+            WaveCountText.text = "Wave: " + waveNumber;
+            timeLeft = firstTimer * waveNumber;
+            
+            StartTimer();
+            
+        }
+
+        }
+    
+        public void StartTimer()
+    {
+        
+            TimerText.text = "Time: " + timeLeft;
+            timeLeft -= Time.deltaTime;       
+        
+           if(timeLeft < 0)
+            {
+            //WAVE END
+            WaveActive = false;
+
+            }
+    }
+    
+}
